4795aa32e4c8068516329ea90a1575df13b31b39
Schemen <schemen@users.noreply.github.com>
2022-11-02 13:20:19 +0100

    Release of version 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='privacycow',
-    version='0.0.5',
+    version='0.1.0',
     packages=["privacycow"],
     include_package_data=True,
     install_requires=[
